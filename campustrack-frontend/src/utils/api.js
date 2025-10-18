6381bb2c2d587254fb9857a13546f684534b6e3d
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2025-10-18 15:41:43 +0530

    Delete campustrack-frontend directory

--- campustrack-frontend/src/utils/api.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-export const apiPost = (url, data) => axios.post(`http://localhost:8080${url}`, data);
-export const apiGet = (url) => axios.get(`http://localhost:8080${url}`);
